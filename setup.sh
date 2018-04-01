d059c95fe892b3669e3b20026d7943eaa34effe1
Musica72 <paula.gubina@gmail.com>
2018-04-01 17:58:39 +0200

    Setup 2.0

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
+#!/usr/bin/env bash
 set -o errexit
-     
+
+download="$(wget https://wordpress.org/latest.tar.gz)"
+unzip="$(tar -xvzf latest.tar.gz)" 
+home="/home/${USER}"  #global variable $HOME exists, but i think this is more appropriate.
+repo="${home}/WREP"
+get_repo="$(git clone https://github.com/Musica72/WREP.git)" 
+ngdir="/usr/share/nginx/"
+confs="${repo}/CONFS"
+ndef="/etc/nginx/sites-available/"
+phpdef="/etc/php/7.2/fpm/pool.d/"
+wploc="${ngdir}/wordpress/"
+
 # much nicer solution / root user id is always 0 (or sudo when privileges are elevated) -> 'exec' is dangerous!
 # script must be started with sudo privileges or root user
 if [ "$(id -u)" -ne 0 ]
@@ -9,24 +21,17 @@ then
 fi
      
 # define functions -> apt-get check is fine, but not needed since package names can vary on different linux distributions (hence, this must be run on ubuntu/debian where apt-get is present)
-    install() {
-        apt-get install "${1}"
-    }
-    add_repo() {
-        apt-add-repository -y install ppa:"${1}"
-    }
-    update(){
-        apt-get update
-    }
-    upgrade(){
-        apt-get upgrade
-    }
-     
-# wget is the name of the command -> name functions with valid and resonable names
-get_latest_wp() {
-    cd ~ # its better to have an absolute pat to folder, also /tmp or others are more approppriate then /root...
-    wget https://wordpress.org/latest.tar.gz # also variables...use them, define them on top of the script
-    tar -xvzf latest.tar.gz
+install() {
+    apt-get install "${1}"
+}
+add_repo() {
+    apt-add-repository -y install ppa:"${1}"
+}
+update(){
+    apt-get update
+}
+upgrade(){
+    apt-get upgrade
 }
      
 # add package repositories - add-apt-repository only makes package available for download, refreshes the package list -> does not install them!
@@ -35,6 +40,14 @@ add_repo install ondrej/php7.2-fpm
      
 # only needed once after repositories are added -> to refresh package list
 update
+
+# get Wordpress
+get_latest_wp() {
+    cd ${home}
+    ${download}
+    ${unzip}
+    cp "${home}/wordpress" ${ngdir}
+}
      
 # install packages
 install nginx
@@ -54,6 +67,16 @@ echo "Proxy server is installed."
    
 install bind9
 echo "DNS server is installed."
+
+# Get configuration files from Git repository (with assumption that GIT was pre-installed on the system)
+cd ${home}
+${get_repo}
+
+# Copy configuration files 
+cp $confs/www.conf ${phpdef}
+cp $confs/default  ${ndef}
+cp $confs/wp-config.php ${wploc}
+
     
 # all good, exit now
 exit 0
