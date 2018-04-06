93843876b1073fcfdd3618b5ff487561f4e93f30
Musica72 <32735890+Musica72@users.noreply.github.com>
2018-04-06 12:47:29 +0200

    Setup 4
    Newest modification for setup

--- setup.sh
+++ setup.sh
@@ -9,39 +9,35 @@ then
 fi
 
 # Variables
-<<<<<<< HEAD
-getwp="$(wget https://wordpress.org/latest.tar.gz)"
-untar="$(tar -xvzf latest.tar.gz)" 
-=======
-download="$(wget https://wordpress.org/latest.tar.gz)"
-unzip="$(tar -xvzf latest.tar.gz)" 
->>>>>>> 6d6a8448666758e1c07270e8d7411cf043752438
-home="/home/${USER}"  #global variable $HOME exists, but i think this is more appropriate.
+wpurl='https://wordpress.org/latest.tar.gz)'
+wpfile='latest.tar.gz' 
+home="/root"  #global variable $HOME exists, but i think this is more appropriate.
 repo="${home}/WREP"
 ngdir="/usr/share/nginx/"
 confs="${repo}/CONFS"
 ndef="/etc/nginx/sites-available/"
 phpdef="/etc/php/7.2/fpm/pool.d/"
+wpdir="${home}/wordpress"
 wploc="${ngdir}/wordpress/"
 dnsloc="/etc/bind/"
     
 # define functions -> apt-get check is fine, but not needed since package names can vary on different linux distributions (hence, this must be run on ubuntu/debian where apt-get is present)
 install() {
-    apt-get install "${1}"
+    apt-get install -qq"${1}"
 }
 add_repo() {
-    apt-add-repository -y install ppa:"${1}"
+    apt-add-repository -y ppa:"${1}" > /dev/null 2>&1
 }
 update(){
-    apt-get update
+    apt-get --qq update
 }
 upgrade(){
-    apt-get upgrade
+    apt-get -qq upgrade
 }
      
 # add package repositories - add-apt-repository only makes package available for download, refreshes the package list -> does not install them!
 add_repo nginx/stable
-add_repo install ondrej/php7.2-fpm
+add_repo ondrej/php
      
 # only needed once after repositories are added -> to refresh package list
 update
@@ -50,10 +46,12 @@ update
 install nginx
 echo "NGINX is installed."
      
-install mysql5.6-server
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server-5.6 mysql-server/root_password_again password root'
+install mysql-server-5.6
 echo "MYSQL is installed."
      
-install php7.2-fpm
+install php7.2-fpm 
 echo "PHP-fpm is installed."
      
 install squid
@@ -65,26 +63,28 @@ echo "DNS server is installed."
 # get Wordpress
 get_latest_wp() {
     cd "${home}"
-    "${getwp}"
-    "${untar}"
-    cp "${home}/wordpress" ${ngdir}
+    wget "${wpurl}"
+    tar -xvzf "${wpfile}"
+    cp "${wpdir}" "${ngdir}"
 }
 
 get_latest_wp 
 echo "Wordpress is installed."
 
 # Copy configuration files 
-<<<<<<< HEAD
-cp "${confs}/www.conf" ${phpdef}
-cp "${confs}/default"  ${ndef}
-cp "${confs}/wp-config.php" ${wploc}
+cp "${confs}/www.conf" "${phpdef}"
+cp "${confs}/default"  "${ndef}"
+cp "${confs}/wp-config.php" "${wploc}"
 cp -avr "${repo}" "${dnsloc}"
-=======
-cp ${confs}/www.conf ${phpdef}
-cp ${confs}/default  ${ndef}
-cp ${confs}/wp-config.php ${wploc}
-cp -avr ${repo} ${dnsloc}
->>>>>>> 6d6a8448666758e1c07270e8d7411cf043752438
+
+# Removing extra files
+del () {
+ rm "${dnsloc}/setup.sh"
+ rm -rf "${wpfile}"
+ rm -rf "${wpdir}"
+ rm -rf "${repo}"
+}
+
    
 # Allow firewall
 firewall (){
@@ -94,6 +94,7 @@ firewall (){
  ufw allow 80 #http
  ufw allow 9000 #for php
  ufw allow 53 #if connects to dns
+ ufw enable 
 }
 
 firewall
